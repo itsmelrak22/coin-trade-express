cb0a32cc6c2d9fe9706c248d47f077733b3ca3cb
Karl Angelo Oriel <itsmelrak22@gmail.com>
2023-06-24 22:22:29 +0800

    0624

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,14 +13,14 @@ import io from 'socket.io-client';
 import MyMixins from './MyMixins'
 
 
-const socket = io("https://cointradeexpress.com:5000");
+const socket = io("cointradeexpress.com:5000");
 
 Vue.prototype.$socket = socket;
 import VueSocketIO from 'vue-socket.io'
 
 Vue.use(new VueSocketIO({
     // debug: false,
-    connection: io('https://cointradeexpress.com:5000'), //options object is Optional
+    connection: io('cointradeexpress.com:5000'), //options object is Optional
     vuex: {
         store,
         actionPrefix: "SOCKET_",
